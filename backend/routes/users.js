e69519984adcaeb15d03b7cf3ec89c6878f699a1
Kevin2.0 <163003196+Kode-god@users.noreply.github.com>
2025-03-31 14:37:54 +0300

    Delete backend directory

--- backend/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCurrentUser, getUserProfile } = require('../controllers/userController');
-const auth = require('../middleware/auth');
-
-router.get('/me', auth, getCurrentUser);
-router.get('/:id', auth, getUserProfile);
-
-module.exports = router;
